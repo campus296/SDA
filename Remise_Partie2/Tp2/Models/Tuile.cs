d9904e26e039f577fa465d6014aec4cfba1ea448
SMI\2033511 <2033511@etu.cchic.ca>
2026-09-23 11:44:58 -0400

    no message

--- Remise_Partie2/Tp2/Models/Tuile.cs
+++ Remise_Partie2/Tp2/Models/Tuile.cs
@@ -1,6 +1,13 @@
-﻿namespace Tp2.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tp2.Models
 {
     public class Tuile
     {
+        public int PositionX;
+        public int PositionY;
+        public int Type;
+        public byte estTraversable;
+        public string imageURL;
     }
 }
